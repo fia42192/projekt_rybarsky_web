e2f867728bd7acb295aaa6e6f2007eb00d307567
fia42192 <fia42192@spseol.cz>
2024-03-27 23:26:04 +0100

    Přidání buttonů a trochu zlepšení vzhledu stránky

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
   }
+
+
+  var reviryIds = ['revir1', 'revir2', 'revir3', 'revir4', 'revir5'];
+  var currentRevirIndex = 0;
+  
+  function changeRevir(direction) {
+    document.getElementById(reviryIds[currentRevirIndex]).classList.remove('active');
+  
+    currentRevirIndex += direction;
+    if (currentRevirIndex < 0) {
+      currentRevirIndex = reviryIds.length - 1;}
+    else if (currentRevirIndex > reviryIds.length - 1) {
+      currentRevirIndex = 0;
+    }
+  
+    document.getElementById(reviryIds[currentRevirIndex]).classList.add('active');
+  }
+  
+  window.onload = function() {
+    document.getElementById(reviryIds[currentRevirIndex]).classList.add('active');
+  };
